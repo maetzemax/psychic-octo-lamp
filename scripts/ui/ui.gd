eefbb25f86920d71dd8f56a1658b17635ebaac50
Max Maetze <maetze.max@icloud.com>
2025-05-06 00:30:55 +0200

    Added: Ingame Overlay

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -1,20 +1,34 @@
 extends CanvasLayer
 
 @onready var death_menu: Control = $DeathScreen
-
 @onready var pause_menu: Control = $PauseMenu
+@onready var ingame_overlay: Control = $IngameOverlay
+var health_bar: ProgressBar
 
-func _process(delta: float):
+var player: CharacterBody2D
+
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
+	health_bar = ingame_overlay.get_node("ProgressBar")
+	health_bar.max_value = player.data.health
+	health_bar.value = player.data.health
+
+func _process(_delta: float):
 	match GameManager.active_game_state:
 		GameManager.DEATH:
 			death_menu.visible = true
 			pause_menu.visible = false
+			ingame_overlay.visible = false
 		GameManager.PAUSE:
 			death_menu.visible = false
 			pause_menu.visible = true
-		_:
+			ingame_overlay.visible = false
+		GameManager.FIGHTING:
 			death_menu.visible = false
 			pause_menu.visible = false
+			ingame_overlay.visible = true
+			
+			health_bar.value = player.data.health
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey:
