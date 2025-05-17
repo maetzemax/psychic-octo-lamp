f29057644bc49773cad4afd2e5e8058f6ab4a249
Max Maetze <maetze.max@icloud.com>
2025-05-17 17:08:17 +0200

    Added: Stats + UpgradeButtons

--- ui/scripts/stats/player_stats_ui.gd
+++ ui/scripts/stats/player_stats_ui.gd
@@ -1,12 +1,16 @@
 extends Control
 
+@export var is_round_end: bool = false
+
 var player: CharacterBody2D
 
-@onready var _material_label: Label = $MarginContainer/VBoxContainer/Material/HBoxContainer/Amount
-@onready var _health_label: Label = $MarginContainer/VBoxContainer/Health/HBoxContainer/Amount
-@onready var _armor_label: Label = $MarginContainer/VBoxContainer/Armor/HBoxContainer/Amount
-@onready var _move_speed_label: Label = $MarginContainer/VBoxContainer/MovementSpeed/HBoxContainer/Amount
-@onready var _attack_damage_label: Label = $MarginContainer/VBoxContainer/AttackDamage/HBoxContainer/Amount
+@onready var _material_label: Label = $MarginContainer/VBoxContainer/Material/Panel/HBoxContainer/Amount
+@onready var _health_label: Label = $MarginContainer/VBoxContainer/Health/Panel/HBoxContainer/Amount
+@onready var _armor_label: Label = $MarginContainer/VBoxContainer/Armor/Panel/HBoxContainer/Amount
+@onready var _attack_damage_label: Label = $MarginContainer/VBoxContainer/AttackDamage/Panel/HBoxContainer/Amount
+@onready var _attack_speed_label: Label = $MarginContainer/VBoxContainer/AttackSpeed/Panel/HBoxContainer/Amount
+@onready var _attack_range_label: Label = $MarginContainer/VBoxContainer/AttackRange/Panel/HBoxContainer/Amount
+@onready var _move_speed_label: Label = $MarginContainer/VBoxContainer/MovementSpeed/Panel/HBoxContainer/Amount
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
@@ -17,4 +21,17 @@ func _process(delta: float) -> void:
 		_health_label.text = str(player.data.current_health)
 		_armor_label.text = str(player.data.armor)
 		_attack_damage_label.text = str(player.data.damage)
+		_attack_speed_label.text = str(int(player.data.attack_speed) * 100) + " %"
+		_attack_range_label.text = str(int(player.data.attack_range) * 100) + " %"
 		_move_speed_label.text = str(int(player.data.movement_speed) * 100) + " %"
+	
+	if !is_round_end:
+		get_tree().call_group_flags(
+			SceneTree.GROUP_CALL_DEFERRED | SceneTree.GROUP_CALL_REVERSE,
+			"UpgradeButton", "hide"
+		)
+	else:
+		get_tree().call_group_flags(
+			SceneTree.GROUP_CALL_DEFERRED | SceneTree.GROUP_CALL_REVERSE,
+			"UpgradeButton", "show"
+		)
