6cc64109a6d0eabee9001cc251b594e6266f9ed6
Max Maetze <max.maetze@develappers.de>
2025-06-03 21:57:32 +0200

    Added: MaterialService signal usage

--- ui/scripts/stats/material_stats_row_ui.gd
+++ ui/scripts/stats/material_stats_row_ui.gd
@@ -1,5 +1,9 @@
 extends StatsRowBase
 
-func _process(_delta: float) -> void:
+func _ready():
+	super._ready()
 	if material_service:
-		amount_label.text = str(material_service.get_materials())
+		material_service.materials_changed.connect(_on_material_changed)
+
+func _on_material_changed(newCount: int):
+	amount_label.text = str(newCount)
